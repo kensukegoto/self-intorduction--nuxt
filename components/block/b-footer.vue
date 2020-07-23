d4443202bdf2bd36c51318bdafddfda998a4f003
kensuke goto <kensuke.goto.1981@gmail.com>
2020-07-23 10:23:59 +0900

    追加：フッター作成

---
+++ components/block/b-footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="b-footer">
+    <p>©️KENSUKE GOTO</p>
+  </footer>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.b-footer{
+  height: 80px;
+  background-color: #333;
+  background-image: url(/image/footer.svg);
+  background-position-y: center;
+  background-position-x: -50px;
+  background-repeat: repeat-x;
+  background-size: 600px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  font-weight: bold;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="container">
-    <bHeader />
+    <!-- <bHeader /> -->
+    <bFooter />
   </div>
 </template>
 
 <script>
 import bHeader from '~/components/block/b-header';
+import bFooter from '~/components/block/b-footer';
 
 export default {
   components: {
-    bHeader
+    bHeader,
+    bFooter
   }
 }
 </script>
